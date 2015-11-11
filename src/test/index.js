a3be8eb4ee19c64804b254bca2169df1beb60ee3
Jesse Cogollo <cogollo87@gmail.com>
2015-11-10 22:49:11 -0500

    update unit test

--- src/test/index.js
+++ src/test/index.js
@@ -1,15 +1,15 @@
 var Magento = require('../magento');
 
-if (!process.env.MAGENTO_HOST) throw new Error('Host not present in environment variable MAGENTO_HOST');
-if (!process.env.MAGENTO_USER) throw new Error('Username not present in environment variable MAGENTO_USER');
-if (!process.env.MAGENTO_PASS) throw new Error('Password not present in environment variable MAGENTO_PASS');
+//if (!process.env.MAGENTO_HOST) throw new Error('Host not present in environment variable MAGENTO_HOST');
+//if (!process.env.MAGENTO_USER) throw new Error('Username not present in environment variable MAGENTO_USER');
+//if (!process.env.MAGENTO_PASS) throw new Error('Password not present in environment variable MAGENTO_PASS');
 
 var magento = new Magento({
-  host: process.env.MAGENTO_HOST,
+  host: process.env.MAGENTO_HOST || 'virtual',
   port: 80,
   path: '/api/xmlrpc/',
-  login: process.env.MAGENTO_USER,
-  pass: process.env.MAGENTO_PASS
+  login: process.env.MAGENTO_USER || 'magento',
+  pass: process.env.MAGENTO_PASS || 'test4echo'
 });
 
 
