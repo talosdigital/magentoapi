9e1c81b35b87a5d3c1bb2dfa513311ce67533110
Ricardo Lara <riclara@gmail.com>
2015-12-01 16:49:55 -0500

    Start create test for new api

--- src/test/index.js
+++ src/test/index.js
@@ -1,4 +1,6 @@
+var assert = require('assert');
 var Magento = require('../magento');
+var wagner = require('wagner-core');
 
 if (!process.env.MAGENTO_HOST) throw new Error('Host not present in environment variable MAGENTO_HOST');
 if (!process.env.MAGENTO_USER) throw new Error('Username not present in environment variable MAGENTO_USER');
@@ -12,28 +14,64 @@ var magento = new Magento({
   pass: process.env.MAGENTO_PASS
 });
 
+var safe = wagner.safe();
 
-console.log('Testing login');
-magento.login(function(err, sessionId) {
-  if (err) throw err;
 
-  console.log(sessionId);
-
-  console.log('Changing session id and trying');
-  magento.login(function(err, newSessionId) {
-    if (err) throw err;
-
-    magento.changeSession(sessionId);
-    console.log('Changed session from %s to %s', newSessionId , sessionId);
-
-    magento.directoryCountry.list(function(err, countries) {
-      if (err) throw err;
-      console.log('Found %d countries', countries.length);
-    });
-
-    magento.bighippoProducts.listSimpleProducts({productId:110},function(err, simpleProduct) {
-      if (err) throw err;
-      console.log('Found %d simpleProducts', simpleProduct);
-    });
+before(function() {
+  //Factory login
+  wagner.factory('login', function(){
+    magento.login(safe.try(
+      function(err, newSessionId) {
+        if (err) throw err;
+        magento.changeSession(sessionId);
+      }
+    ));
+  });
+  //invoke login
+  wagner.invoke(function(login){
+    console.log('end invoke1')
   });
 });
+
+after(function() {
+  // runs after all tests in this block
+});
+
+safe.on('error', function(error) {
+  //assert.equal(error.toString(), 'Oops I messed up');
+  console.log('error' , error.toString());
+  done();
+});
+
+
+describe('new api products',function(){
+
+  it('list Simple Products error missing value for productId', function (done) {
+    magento.bighippoProducts.listSimpleProducts({}, function(err,data){
+      assert.equal('missing value for "productId"', err.message)
+      assert.equal('listSimpleProducts', err.method)
+      done();
+    });
+  });
+
+  it('list Simple Products', function (done) {
+
+    magento.bighippoProducts.listSimpleProducts({productId:105, arguments: [], includeMedia :true}, function(err,data){
+      console.log('err: ', err);
+      console.log('data', data);
+
+
+      if(err) return done(err);
+      assert.isNotNull(data);
+      done();
+    });
+  });
+
+
+
+
+});
+
+
+
+
